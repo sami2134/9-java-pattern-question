aa04f405bd1599c8148b015947e193f26884333d
sami <106396315+sami2134@users.noreply.github.com>
2023-07-31 14:41:20 +0530

    Update half_pyramid_number.java

--- src/half_pyramid_number.java
+++ src/half_pyramid_number.java
@@ -1,3 +1,11 @@
+half pyramid number:
+1
+1 2    
+1 2 3
+1 2 3 4
+1 2 3 4 5
+    to print this pattern following is the code--
+
 public class half_pyramid_number {
     public static void main(String args[]){
         int n=5;
